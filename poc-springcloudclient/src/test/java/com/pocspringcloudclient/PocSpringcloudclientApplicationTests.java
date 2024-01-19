a5a2b6a89a8f079324bf07fd9e7c555dd1aa4fa4
ramverma-github <verma.rb91@gmail.com>
2024-01-19 22:17:44 +0530

    Added code to setup spring cloud config server access to spring boot application

---
+++ poc-springcloudclient/src/test/java/com/pocspringcloudclient/PocSpringcloudclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocspringcloudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringcloudclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
